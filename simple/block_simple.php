6ab52c1dc28c0b8fdd8885f3c5f38a9d239af72f
arif rohman hakim <arifrohman@tonjoo.com>
2017-05-13 11:47:30 +0700

    update simple block

--- simple/block_simple.php
+++ simple/block_simple.php
@@ -3,4 +3,15 @@ class block_simple extends block_base {
     public function init() {
         $this->title = get_string('simple', 'block_simple');
     }
+    public function get_content() {
+    	if ($this->content !== null) {
+           return $this->content;
+        }
+ 
+        $this->content         =  new stdClass;
+        $this->content->text   = $this->config->text;
+        $this->content->footer = '';
+ 
+        return $this->content;
+    }
 }
